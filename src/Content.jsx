cc463c7834367580b903ad49081453af38bfc7aa
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-08-13 11:52:28 -0500

    Makes more info modal button work

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,6 +6,7 @@ import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
 
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
@@ -13,13 +14,28 @@ export function Content() {
       console.log(response.data);
     });
   };
+
+  const handleShowPost = (post) => {
+    setIsPostsShowVisible(true);
+    setCurrentPost(post);
+    console.log(post);
+  };
+  const handleClose = () => {
+    setIsPostsShowVisible(false);
+  };
+  const [currentPost, setCurrentPost] = useState({});
+
   useEffect(handleIndexPosts, []);
   return (
     <div>
       <PostsNew />
-      <PostsIndex posts={posts} />
-      <Modal show={true}>
-        <p>TEST</p>
+      <PostsIndex posts={posts} onShowPost={handleShowPost} />
+      <Modal show={isPostsShowVisible} onClose={handleClose}>
+        <div>
+          <h2>Title: {currentPost.title}</h2>
+          <p>Body: {currentPost.body}</p>
+          <p>Image: {currentPost.image}</p>
+        </div>
       </Modal>
     </div>
   );
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -7,7 +7,7 @@ export function PostsIndex(props) {
           <h2>{post.title}</h2>
           <img src={post.image} alt="" />
           <p>Body: {post.body}</p>
-          <button>More Info</button>
+          <button onClick={() => props.onShowPost(post)}>More Info</button>
         </div>
       ))}
     </div>
