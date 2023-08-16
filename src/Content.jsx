ad2244d4e7d694bb6c9cac53fb4cb27dbe4c8972
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-08-16 18:09:47 -0500

    Update moved to content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -26,6 +26,20 @@ export function Content() {
     });
   };
 
+  const handleUpdatePost = (id, params) => {
+    axios.patch(`http://localhost:3000/posts/${id}.json`, params).then((response) => {
+      setPosts(
+        posts.map((post) => {
+          if (post.id === response.data.id) {
+            return response.data;
+          } else {
+            return post;
+          }
+        })
+      );
+    });
+  };
+
   const handleShowPost = (post) => {
     setIsPostsShowVisible(true);
     setCurrentPost(post);
@@ -45,7 +59,7 @@ export function Content() {
       <PostsNew onCreatePost={handleCreatePost} />
       <PostsIndex posts={posts} onShowPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
-        <PostsShow post={currentPost} />
+        <PostsShow post={currentPost} onUpdatePost={handleUpdatePost} closeModal={handleClose} />
       </Modal>
     </div>
   );
